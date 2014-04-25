1dec3fbcee86229a64f8738da13321f071e62ef6
geraldstanje <Gerald.Stanje@gmail.com>
2014-04-25 16:23:28 +0200

    formatted code

--- src/sbmpfont.h
+++ src/sbmpfont.h
@@ -10,8 +10,8 @@ public:
   ~SBMPFont();
         
   void init(const char* fontFile);
-
   void printAt(float x, float y, const char* text, bool second=false);
+  
 protected:
   SBMPTexture texture;
   GLuint lists;
