a196bde4d725cae3fa93b086c1d0ff3e80b55f26
geraldstanje <Gerald.Stanje@gmail.com>
2014-04-25 16:22:34 +0200

    formatted code

--- src/sbmptexture.h
+++ src/sbmptexture.h
@@ -1,38 +1,25 @@
 #ifndef S_BMP_TEXTURE_H
 #define S_BMP_TEXTURE_H
 
-//#include <gl/glew.h>
 #include <GL/glew.h>
 
 class SBMPTexture
 {
 public:
-	void destroy();
+  void destroy();
 	
-	/////////////
-	// Konstruktoren
-	///////////////
-	SBMPTexture();
-	SBMPTexture(const char* filename,int minFilter = GL_LINEAR,
-		int magFilter = GL_LINEAR, bool clampToEdge = false);
-
-
-        virtual ~SBMPTexture();
+  SBMPTexture(const char* filename, int minFilter = GL_LINEAR, int magFilter = GL_LINEAR, bool clampToEdge = false);
+  virtual ~SBMPTexture();
 	
-	/////////////////
-	// Funktionen
-	////////////////
-
-	virtual bool createMipmap(const char* filename,int minFilter = GL_LINEAR_MIPMAP_NEAREST,
-		int magFilter = GL_LINEAR, bool clampToEdge = false);
-	virtual bool createTexture(const char* filename, int minFilter = GL_LINEAR,
-		int magFilter = GL_LINEAR, bool clampToEdge = false);
-	
-	void select();
-
-	GLuint getGLTexture();
+  virtual bool createMipmap(const char* filename,int minFilter = GL_LINEAR_MIPMAP_NEAREST, 
+                            int magFilter = GL_LINEAR, bool clampToEdge = false);
+  virtual bool createTexture(const char* filename, int minFilter = GL_LINEAR,
+                             int magFilter = GL_LINEAR, bool clampToEdge = false);
+  void select();
+  GLuint getGLTexture();
+  
 protected:
-	GLuint texture;
+  GLuint texture;
 };
 
-#endif 
+#endif
